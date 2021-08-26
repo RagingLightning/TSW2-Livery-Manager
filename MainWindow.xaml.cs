a0daf3e3379ea1f581d3eda73eba926c00fa0bcf
RagingLightning <RagingLightningCode@gmail.com>
2021-08-26 12:15:55 +0200

    v0.4.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace TSW2_Livery_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string VERSION = "0.3.2";
+        private const string VERSION = "0.4.0";
 
         //COUNT OF LIVERIES
         readonly byte[] COL = new byte[] { 0x53, 0x74, 0x72, 0x75, 0x63, 0x74, 0x50, 0x72, 0x6f, 0x70, 0x65, 0x72, 0x74, 0x79, 0, 0 };
